3790358718c43974b3208e31be40e45596f6d90e
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-09-11 17:38:59 -0300

    Ajustes pontuais

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,7 +3,7 @@ import { api } from "./api";
 
 export const getCategories = async (): Promise<Category[]> => {
 	try {
-		const response = await api.get("/categories");
+		const response = await api.get("/api/categories");
 
 		return response.data;
 	} catch (error) {
